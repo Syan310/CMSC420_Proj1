c61e658972af5f75d5966134c657ece7d4f415ab
Syan310 <105128575+Syan310@users.noreply.github.com>
2023-09-21 17:37:00 -0400

    final

--- bst.py
+++ bst.py
@@ -56,42 +56,95 @@ def insert(root: Node, key: int) -> Node:
 # If the key is in the tree, decrement its key count. If they keycount goes to 0, remove the key.
 # When replacement is necessary use the inorder successor.
 def delete(root: Node, key: int) -> Node:
-    # YOUR CODE GOES HERE.
+    if root is None:
+        return root
+    
+    if key == root.key:
+        if root.keycount > 1:
+            root.keycount -= 1
+        else:
+            root.keycount == 1
+    elif key > root.key:
+        root.rightchild = delete(root.rightchild, key)
+    else:
+        root.leftchild = delete(root.leftchild, key)
+        
     return root
 
+
+    
 # For the tree rooted at root and the key given:
 # Calculate the list of keys on the path from the root towards the search key.
 # The key is not guaranteed to be in the tree.
 # Return the json.dumps of the list with indent=2.
 def search(root: Node, search_key: int) -> str:
-    # YOUR CODE GOES HERE.
+    list = []
+    curr = root
+    list.append(curr)
+   
+    while curr:
+        list.append(curr.key)
+        
+        if curr.key == search_key:
+            break
+        elif curr.key > search_key:
+            curr = curr.leftchild
+        elif curr.key < search_key:
+            curr = curr.rightchild
+            
     # Then tweak the next line so it uses your list rather than None.
-    return(json.dumps(None))
+    return (json.dumps(list, indent=2))
 
 # For the tree rooted at root, find the preorder traversal.
 # Return the json.dumps of the list with indent=2.
 def preorder(root: Node) -> str:
-    # YOUR CODE GOES HERE.
-    # Then tweak the next line so it uses your list rather than None.
-    return(json.dumps(None))
+    list = []
+    if root is None:
+        return []
+    
+    list =  [root.key] + preorder(root.leftchild) + preorder(root.rightchild)
+    
+    return(json.dumps(list, indent =2))
 
 # For the tree rooted at root, find the inorder traversal.
 # Return the json.dumps of the list with indent=2.
 def inorder(root: Node) -> str:
-    # YOUR CODE GOES HERE.
-    # Then tweak the next line so it uses your list rather than None.
-    return(json.dumps(None))
+    list = []
+    if root is None:
+        return []
+    
+    list =   inorder(root.leftchild) + [root.key] + inorder(root.rightchild)
+    
+    return(json.dumps(list, indent =2))
+
 
 # For the tree rooted at root, find the postorder traversal.
 # Return the json.dumps of the list with indent=2.
 def postorder(root: Node) -> str:
-    # YOUR CODE GOES HERE.
-    # Then tweak the next line so it uses your list rather than None.
-    return(json.dumps(None))
+    list = []
+    if root is None:
+        return []
+    
+    list = postorder(root.leftchild) + postorder(root.rightchild) + [root.key]
+    
+    return(json.dumps(list, indent =2))
 
 # For the tree rooted at root, find the BFT traversal (go left-to-right).
 # Return the json.dumps of the list with indent=2.
 def bft(root: Node) -> str:
-    # YOUR CODE GOES HERE.
-    # Then tweak the next line so it uses your list rather than None.
-    return json.dumps(None)
+    if root is None:
+        return []
+
+    result = []
+    queue = [root]
+
+    while queue:
+        curr = queue.pop(0)  
+        result.append(curr.key)
+
+        if curr.leftchild:
+            queue.append(curr.leftchild)
+        if curr.rightchild:
+            queue.append(curr.rightchild)
+
+    return json.dumps(list, indent=2)
