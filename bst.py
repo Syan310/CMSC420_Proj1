44287863b076e4895215a00fa7c809a4174c8ff5
Syan310 <105128575+Syan310@users.noreply.github.com>
2023-09-23 16:31:55 -0400

    Update bst.py

--- bst.py
+++ bst.py
@@ -14,23 +14,24 @@ def dump(root: Node) -> str:
     #...
     #...
 
+# ... [rest of the provided code]
+
+# 1. Insert function:
 def insert(root: Node, key: int) -> Node:
     if not root:
         return Node(key=key, keycount=1)
-    
-    if key == root.key:
-        root.keycount += 1
-    elif key < root.key:
+    if key < root.key:
         root.leftchild = insert(root.leftchild, key)
-    else:
+    elif key > root.key:
         root.rightchild = insert(root.rightchild, key)
+    else:
+        root.keycount += 1
     return root
 
+# 2. Delete function:
 def delete(root: Node, key: int) -> Node:
     if not root:
         return root
-    
-    # Recursive cases
     if key < root.key:
         root.leftchild = delete(root.leftchild, key)
     elif key > root.key:
@@ -40,61 +41,73 @@ def delete(root: Node, key: int) -> Node:
         if root.keycount == 0:
             if not root.leftchild:
                 return root.rightchild
-            elif not root.rightchild:
+            if not root.rightchild:
                 return root.leftchild
-            temp = get_min(root.rightchild)
-            root.key, root.keycount = temp.key, temp.keycount
+            temp = _inorder_successor(root.rightchild)
+            root.key = temp.key
+            root.keycount = temp.keycount
             root.rightchild = delete(root.rightchild, temp.key)
     return root
 
-def get_min(node):
-    current = node
+def _inorder_successor(root: Node) -> Node:
+    current = root
     while current.leftchild:
         current = current.leftchild
     return current
 
+# 3. Search function:
 def search(root: Node, search_key: int) -> str:
     path = []
     current = root
     while current:
         path.append(current.key)
-        if search_key == current.key:
+        if current.key == search_key:
             break
-        elif search_key < current.key:
+        if search_key < current.key:
             current = current.leftchild
         else:
             current = current.rightchild
     return json.dumps(path, indent=2)
 
+# 4. Preorder traversal:
 def preorder(root: Node) -> str:
-    def traverse(node):
-        if not node:
-            return []
-        return [node.key] + traverse(node.leftchild) + traverse(node.rightchild)
+    result = []
+    def _preorder(node):
+        if node:
+            result.append(node.key)
+            _preorder(node.leftchild)
+            _preorder(node.rightchild)
+    _preorder(root)
+    return json.dumps(result, indent=2)
 
-    return json.dumps(traverse(root), indent=2)
-
+# 5. Inorder traversal:
 def inorder(root: Node) -> str:
-    def traverse(node):
-        if not node:
-            return []
-        return traverse(node.leftchild) + [node.key] + traverse(node.rightchild)
+    result = []
+    def _inorder(node):
+        if node:
+            _inorder(node.leftchild)
+            result.append(node.key)
+            _inorder(node.rightchild)
+    _inorder(root)
+    return json.dumps(result, indent=2)
 
-    return json.dumps(traverse(root), indent=2)
-
+# 6. Postorder traversal:
 def postorder(root: Node) -> str:
-    def traverse(node):
-        if not node:
-            return []
-        return traverse(node.leftchild) + traverse(node.rightchild) + [node.key]
+    result = []
+    def _postorder(node):
+        if node:
+            _postorder(node.leftchild)
+            _postorder(node.rightchild)
+            result.append(node.key)
+    _postorder(root)
+    return json.dumps(result, indent=2)
 
-    return json.dumps(traverse(root), indent=2)
-
+# 7. Breadth-first traversal:
 def bft(root: Node) -> str:
     if not root:
-        return json.dumps([], indent=2)
-    queue = [root]
+        return json.dumps([])
     result = []
+    queue = [root]
     while queue:
         current = queue.pop(0)
         result.append(current.key)
